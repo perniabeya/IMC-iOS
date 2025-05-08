67ef97671768c91823b9fe235d1ef0c75ed3c16e
Perniabeya <abeyabusiness@gmail.com>
2025-05-08 10:32:31 +0200

    Initial Commit

---
+++ IMC-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC-iOS
+//
+//  Created by Mañanas on 8/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
